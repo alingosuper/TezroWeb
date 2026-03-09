87662865a39f175ca5f2c6d6a94ca2e547c965c5
alingosuper <alingosuper@gmail.com>
2026-03-09 05:45:32 +0500

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import React from "react"
+
+function Home(){
+return(
+
+<div>
+
+<section className="hero">
+
+<h1>Tezro Super App</h1>
+
+<p>
+One Platform. Endless Possibilities.
+Tezro Super is a next-generation digital ecosystem designed to connect users, businesses, drivers, vendors, and service providers on a single intelligent platform.
+</p>
+
+<button>Get Started</button>
+
+</section>
+
+
+<section className="about">
+
+<h2>About Tezro</h2>
+
+<p>
+Tezro Super is an advanced multi-service digital platform built to revolutionize the way people access services and run businesses.
+</p>
+
+</section>
+
+</div>
+
+)
+}
+
+export default Home
