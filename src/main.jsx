16792b3f491c61c30745946d7937309472565c4b
alingosuper <alingosuper@gmail.com>
2026-03-25 15:07:46 +0500

    Fix: Removed router import from main.jsx to resolve build error

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import router from './router';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+    <App />
+  </React.StrictMode>,
+)
